c984f2553dd62ef7f1e2ad0f422620dec723bfdb
Duncan Ryan <duncancryan1@gmail.com>
2020-10-04 13:48:35 +0100

    written shop class, first test passing

---
+++ src/main/java/Shop.java
@@ -0,0 +1,31 @@
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
+public class Shop {
+
+    private ArrayList<ISell> stock;
+
+    public Shop() {
+        this.stock = new ArrayList<>();
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public int getStockCount(){
+        return this.stock.size();
+    }
+
+    public void addToStock(ISell stockItem){
+        this.stock.add(stockItem);
+    }
+
+//    have made the below return the removed item in consideration
+//    of what you might want to do with this program in the future
+
+    public ISell removeFromStock(ISell soldItem){
+        return this.stock.remove(this.stock.indexOf(soldItem));
+    }
+}
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,31 @@
+import enums.ReedCount;
+import instruments.Drums;
+import instruments.Flute;
+import instruments.Trumpet;
+import miscellaneousItems.DrumSticks;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    private Shop shop;
+
+    private DrumSticks drumSticks;
+    private Flute flute;
+    private Trumpet trumpet;
+
+    @Before
+    public void before(){
+        shop = new Shop();
+        drumSticks = new DrumSticks("A lovely pair of walnut drum sticks", 8.50, 17.99);
+        flute = new Flute("Nickel", "Silver", 40.00, 89.99, ReedCount.NONE);
+        trumpet = new Trumpet("Brass", "Copper", 81.25, 230.00, true);
+    }
+
+    @Test
+    public void stockStartsEmpty(){
+        assertEquals(0, shop.getStockCount());
+    }
 }
