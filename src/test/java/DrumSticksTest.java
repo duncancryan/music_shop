9494925cb8e336863000313d469e8a4f375edfa8
Duncan Ryan <duncancryan1@gmail.com>
2020-10-04 13:10:42 +0100

    calculate markup just flooring instead of rounding, going to adjust so that it rounds

--- src/test/java/DrumSticksTest.java
+++ src/test/java/DrumSticksTest.java
@@ -1,2 +1,35 @@
+import miscellaneousItems.DrumSticks;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrumSticksTest {
+
+    private DrumSticks drumSticks;
+
+    @Before
+    public void before(){
+        drumSticks = new DrumSticks("A lovely pair of walnut drum sticks", 8.50, 17.99);
+    }
+
+    @Test
+    public void hasDescription(){
+        assertEquals("A lovely pair of walnut drum sticks", drumSticks.getDescription());
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(8.50, drumSticks.getBuyingPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(17.99, drumSticks.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void canCalculateMarkUp(){
+        assertEquals(112, drumSticks.calculateMarkUp());
+    }
 }
