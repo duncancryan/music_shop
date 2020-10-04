5768d83ed761bf3ac35502385ac15adc3c4357ef
Duncan Ryan <duncancryan1@gmail.com>
2020-10-04 02:18:18 +0100

    hasMaterial test passing for drums and therefore all superclasses

--- src/test/java/DrumsTest.java
+++ src/test/java/DrumsTest.java
@@ -1,2 +1,21 @@
+import enums.PercussionMethod;
+import instruments.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrumsTest {
+    private Drums drums;
+
+    @Before
+    public void before(){
+        drums = new Drums("Plastic", "Black", 235.50, 415.99, PercussionMethod.STRIKE);
+    }
+
+    @Test
+    public void hasMaterial(){
+        assertEquals("Plastic", drums.getMaterial());
+    }
 }
+
